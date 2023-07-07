48b20ce0f4e3a19092d75949671d1a96eb7d73e2
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-07 16:35:21 +0200

    initial commit

---
+++ deutsch-c2-quiz-vier/src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Die Lesung der jungen Autorin",
+        answersList: [
+            {answer:"wurde von mehreren Gästen lauthals unterbrochen.", isCorrect:true},
+            {answer:"musste wegen dem einschlagenden Erfolg unterbrochen werden.", isCorrect:false},
+            {answer:"war für einige Zuhörer eine nicht vorhersehbare Unterbrechung.", isCorrect:false},
+        ]
+    },{
+        question:"Nach monatelangen Ermittlungen konnten die Kunsträuber",
+        answersList: [
+            {answer:"von der Polizei sichergestellt werden.", isCorrect:false},
+            {answer:"endlich auf frischer Tat gefasst werden.", isCorrect:true},
+            {answer:"erfolgreich durch die Polizei zerschlagen werden.", isCorrect:false},
+        ]
+    },{
+        question:"Zahlreiche abstrakte Gemälde des Museums",
+        answersList: [
+            {answer:"waren auch für die Experten zu naturalistisch gemalt.", isCorrect:false},
+            {answer:"konnte man nur in unzugänglichen Kabinetten betrachten.", isCorrect:false},
+            {answer:"mussten jahrelang in dunklen Lagerräumen ausharren.", isCorrect:true},
+        ]
+    },{
+        question:"Bei der spektakuären Kunstauktion",
+        answersList: [
+            {answer:"zerstörte der Künstler sein eigenes Werk.", isCorrect:true},
+            {answer:"genoss das Werk keinerlei Aufmerksamkeit.", isCorrect:false},
+            {answer:"mussten alle Abwesenden die Künstlerin bewundern.", isCorrect:false},
+        ]
+    },{
+        question:"Bei der Darbietung seiner Lieder",
+        answersList: [
+            {answer:"merkt man sofort, wohin der Künstler uns tragen will.", isCorrect:false},
+            {answer:"kommt die kulturelle Herkunft des Künstlers zum Tragen.", isCorrect:true},
+            {answer:"trägt der Künstler nicht zur Unterhaltung bei.", isCorrect:false},
+        ]
+    },{
+        question:"Die Inszenierung des Stücks hielt sich",
+        answersList: [
+            {answer:"für die erfolgreiche literarische Vorlage.", isCorrect:false},
+            {answer:"aufgrund der Vorlage an nicht-literarische Grenzen.", isCorrect:false},
+            {answer:"genau an die berühmte literarische Vorlage.", isCorrect:true},
+        ]
+    },{
+        question:"Die Mitglieder des renommierten literarischen Zirkels",
+        answersList: [
+            {answer:"sorgten für einen unvergesslichen Abend.", isCorrect:true},
+            {answer:"mussten am Abend die zahlreichen Gäste versorgen.", isCorrect:false},
+            {answer:"waren vom Inhalt ihrer Darbietung provoziert worden.", isCorrect:false},
+        ]
+    },{
+        question:"Zur Ausstellungseröffnung wird",
+        answersList: [
+            {answer:"die anwesende Künstlerin ihr Projekt unterstützen.", isCorrect:false},
+            {answer:"mit der Anwesenheit der Künstlerin gerechnet.", isCorrect:true},
+            {answer:"das Werk der kritischen Künstlerin geehrt.", isCorrect:false},
+        ]
+    },{
+        question:"Den neuesten Film des Regisseurs solltest du unbedingt gesehen haben.",
+        answersList: [
+            {answer:"Danke für den Hinweis. Ich finde ihn auch schlecht.", isCorrect:false},
+            {answer:"Ich habe leider so gut wie nichts vom Inhalt verstanden.", isCorrect:false},
+            {answer:"Ja, vielen Dank für deine freundliche Empfehlung.", isCorrect:true},
+        ]
+    },{
+        question:"Denken Sie bei Ihrem Kurzreferat daran,",
+        answersList: [
+            {answer:"dass die Zuhörer Ihren Gedankengängen folgen können.", isCorrect:true},
+            {answer:"dass Sie alle Aspekte des Werkes ausführlich behandeln.", isCorrect:false},
+            {answer:"dass Ihr persönlicher Standpunkt immer im Vordergrund steht.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Eine Hand wäscht die andere, sagt ein Sprichwort.",
+        answersList: [
+            {answer:"Leider ist Bestechlichkeit immer noch weit verbreitet.", isCorrect:true},
+            {answer:"Kavaliersdelikte machen unsere Gesellschaft liebenswert.", isCorrect:false},
+            {answer:"In allen modernen Gesellschaften ist Korruption notwendig.", isCorrect:false},
+        ]
+    },{
+        question:"Angesichts der großen Altersunterschiede wurden die Teilnehmer in Gruppen aufgeteilt.",
+        answersList: [
+            {answer:"Deshalb gibt es jetzt nur noch erwachsene Teilnehmer.", isCorrect:false},
+            {answer:"Die Teilnehmenden wurden in verschiedene Altersgruppen eingeteilt.", isCorrect:true},
+            {answer:"Aus diesem Grund müssen alle Teilnehmenden vormittags kommen.", isCorrect:false},
+        ]
+    },{
+        question:"Anhand der gerichtsmedizinischen Beweise",
+        answersList: [
+            {answer:"forderte der Angeklagte verurteilt zu werden.", isCorrect:false},
+            {answer:"wurden die Rechtsanwälte zu Rate gezogen.", isCorrect:false},
+            {answer:"konnte der Tathergang rekonstruiert werden.", isCorrect:true},
+        ]
+    },{
+        question:"Die Urteilsverkündung sollte binner kürzester Zeit erfolgen.",
+        answersList: [
+            {answer:"Wir rechnen in Kürze mit der Bekanntgabe des Urteils.", isCorrect:true},
+            {answer:"Die Richter werden sich für den Prozess Zeit nehmen.", isCorrect:false},
+            {answer:"Gehen Sie davon aus, dass es noch länger dauert.", isCorrect:false},
+        ]
+    },{
+        question:"Entgegen aller Erwartungen konnte die Sängerin ihr Konzert geben.",
+        answersList: [
+            {answer:"Wir haben gehofft, dass die Künstlerin das Konzert absagt.", isCorrect:false},
+            {answer:"Wir befürchteten, dass das Konzert abgesagt werden würde.", isCorrect:true},
+            {answer:"Uns erschien es unpassend, die Besucher zu informieren.", isCorrect:false},
+        ]
+    },{
+        question:"Dank Ihrer Unterstützung konnte ich das Projekt erfolgreich durchführen.",
+        answersList: [
+            {answer:"Obwohl Sie mir Hilfe angeboten haben, konnte ich das Projekt nicht beenden.", isCorrect:false},
+            {answer:"Vielen Dank für Ihre Hilfsbereitschaft. Das Projekt ist zu Ende.", isCorrect:false},
+            {answer:"Ohne Ihre Hilfe hätte das Projekt keinen Erfolg gehabt.", isCorrect:true},
+        ]
+    },{
+        question:"Laut Ihrem Arbeitsvertrag haben Sie Anrecht auf Reisekostenerstattung.",
+        answersList: [
+            {answer:"Die Kosten für Dienstreisen werden vom Arbeitgeber erstattet.", isCorrect:true},
+            {answer:"Die Reisekosten müssten eigentlich vom Arbeitgeber bezahlt werden.", isCorrect:false},
+            {answer:"Der Arbeitgeber könnte Ihnen die Reisekosten zurückbezahlen.", isCorrect:false},
+        ]
+    },{
+        question:"Mangels finanzieller Unterstützung musste das Konzert abgesagt werden.",
+        answersList: [
+            {answer:"Obwohl die Leute uns bezahlten, mussten wir das Konzert abblasen.", isCorrect:false},
+            {answer:"Weil wir nicht genug Sponsoren finden konnten, fiel das Konzert aus.", isCorrect:true},
+            {answer:"Da die Organisatoren kein Geld hatten, wurde der Eintritt verteuert.", isCorrect:false},
+        ]
+    },{
+        question:"Dem Pressesprecher zufolge ist das Ministerium nicht zuständig.",
+        answersList: [
+            {answer:"Die Ministerin hat die Verantwortung übernommen.", isCorrect:false},
+            {answer:"Der Pressesprecher repräsentiert nicht die Ministerin.", isCorrect:false},
+            {answer:"Das Ministerium wird sich nicht mit der Angelegenheit befassen.", isCorrect:true},
+        ]
+    },{
+        question:"Vonseiten des Betriebsrates wurden sofortige Lohnerhöhungen gefordert.",
+        answersList: [
+            {answer:"Der Betriebsrat verlangt höhrere Löhne für die Belegschaft.", isCorrect:true},
+            {answer:"Der Arbeitgeber ist nicht bereit, die Gehälter langsam anzuheben.", isCorrect:false},
+            {answer:"Die Arbeitnehmer unterstützen die Forderungen des Betriebsrates nicht.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Zwecks Gründung einer Firma",
+        answersList: [
+            {answer:"wenden Sie sich bitte an unsere Unternehmensberatung.", isCorrect:true},
+            {answer:"musste ich an einem Erste-Hilfe-Kurs teilnehmen.", isCorrect:false},
+            {answer:"wollte ich mich freiwillig sozial engagieren.", isCorrect:false},
+        ]
+    },{
+        question:"Die Idee mich selbständig zu machen",
+        answersList: [
+            {answer:"musste ich nach Prüfung aller Möglichkeiten ablehnen.", isCorrect:false},
+            {answer:"habe ich nach langem Zögern schließlich verworfen.", isCorrect:true},
+            {answer:"konnte ich trotz finanzieller Unterstützung nicht wahrnehmen.", isCorrect:false},
+        ]
+    },{
+        question:"Bei riskanten Kapitalanlagen sollten Sie",
+        answersList: [
+            {answer:"alles auf eine Karte setzen und schnell handeln.", isCorrect:false},
+            {answer:"Hals über Kopf und ohne Rücksicht auf Verluste handeln.", isCorrect:false},
+            {answer:"Vorsicht walten lassen und nichts überstürzen.", isCorrect:true},
+        ]
+    },{
+        question:"Der Sicherheit halber empfiehlt die Bank",
+        answersList: [
+            {answer:"ihren Kunden die Zwei-Faktor-Authentisierung.", isCorrect:true},
+            {answer:"dass Sie sich persönlich und virtuell identifizieren.", isCorrect:false},
+            {answer:"bei Überweisungen eine Erlaubnis der Bank einzuholen.", isCorrect:false},
+        ]
+    },{
+        question:"Aufgrund der plötzlichen Wetteränderung",
+        answersList: [
+            {answer:"ist der Sänger ins Wasser gefallen.", isCorrect:false},
+            {answer:"musste das Konzert ins Freie verlegt werden.", isCorrect:false},
+            {answer:"ist das Konzert ins Wasser gefallen.", isCorrect:true},
+        ]
+    },{
+        question:"Er dachte, er würde alles im Griff haben,",
+        answersList: [
+            {answer:"aber dann hat er den Überblick verloren.", isCorrect:true},
+            {answer:"denn er hat zwei linke Hände.", isCorrect:false},
+            {answer:"weil sein Projekt weder Hand noch Fuß hatte.", isCorrect:false},
+        ]
+    },{
+        question:"Nach mehreren persönlichen Rückschlägen",
+        answersList: [
+            {answer:"hat sie jetzt nicht mehr viel um die Ohren.", isCorrect:false},
+            {answer:"hat sie ihr Schicksal in die eigenen Hände genommen.", isCorrect:true},
+            {answer:"nahm sie die Beine in die Hand und lief weg.", isCorrect:false},
+        ]
+    },{
+        question:"Du hättest beim Verfassen der Reklamation",
+        answersList: [
+            {answer:"die Zustände im Kundenservice lobend hervorheben sollen.", isCorrect:false},
+            {answer:"am besten auch auf dein Verschulden hinweisen können.", isCorrect:false},
+            {answer:"auch an eine Rückforderung der Kosten denken sollen.", isCorrect:true},
+        ]
+    },{
+        question:"Noch bevor sie ihre Ausbildung abgeschlossen hatte,",
+        answersList: [
+            {answer:"wehten ihr schon die Stellenangebote ins Haus.", isCorrect:true},
+            {answer:"machte sie sich daran, einen Ausbildungsplatz zu suchen.", isCorrect:false},
+            {answer:"wollte sie schon als Assistenzärztin selbständig operieren.", isCorrect:false},
+        ]
+    },{
+        question:"Zwei Mitarbeiter wurden gestern gefeuert.",
+        answersList: [
+            {answer:"Wir waren schlauer. Das hätte uns nicht passieren können.", isCorrect:false},
+            {answer:"Sie waren schon länger für eine Beförderung vorgesehen.", isCorrect:false},
+            {answer:"Ja, es war eine knallharte Entscheidung des Personalschefs.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
